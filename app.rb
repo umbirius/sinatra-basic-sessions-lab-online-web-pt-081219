787052a3b7782e692e477d31c84e77f669c8c073
Umberto Palazzo <upalazzo93@gmail.com>
2019-11-25 21:45:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
+require "pry"
 
 class App < Sinatra::Base
+  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+  
+  get "/" do 
+    erb :index
+  end 
+  
+  post "/checkout" do 
+    @sessions = session
+    item = params["item"]
+    binding.pry
+    @sessions[:item] = item
+  end 
 end
